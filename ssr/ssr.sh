f34cd483f7dc5d34085fbd33445012963ca62e13
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-02-27 18:19:02 +0200

    Update ssr.sh

--- ssr/ssr.sh
+++ ssr/ssr.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(curl -s -X GET https://checkip.amazonaws.com);
 echo "Checking VPS"
-IZIN=$( curl http://miss.my.id:3009/api/v1/users | grep -o "$MYIP" );
+IZIN=$( curl https://raw.githubusercontent.com/Rolka111111/scripts/main/users | grep -o "$MYIP" );
 if [ $MYIP = $IZIN >/dev/null 2>&1 ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -25,7 +25,7 @@ echo -e "${NC}${LIGHT}Telegram : https://t.me/mochammadhb"
 exit 0
 fi
 # Link Hosting Kalian
-hasandnc="raw.githubusercontent.com/mochammadhb/scripts/main/ssr"
+hasandnc="raw.githubusercontent.com/Rolka111111/scripts/main/ssr"
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
