e9a58bc6034b8d83079753e7574e78d3c7ca2087
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-02-27 18:02:58 +0200

    Update addv2ray.sh

--- xray/addv2ray.sh
+++ xray/addv2ray.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 MYIP=$(curl -s -X GET https://checkip.amazonaws.com);
 MYIP=$(curl -s -X GET https://checkip.amazonaws.com);
 echo "Checking VPS"
-IZIN=$( curl curl https://raw.githubusercontent.com/Rolka111111/scripts/main/users | grep -o "$MYIP" );
+IZIN=$( curl https://raw.githubusercontent.com/Rolka111111/scripts/main/users | grep -o "$MYIP" );
 if [ $MYIP = $IZIN >/dev/null 2>&1 ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
