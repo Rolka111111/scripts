e35c7bde350c20a4e551a048757ba6518f0de4d7
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-02-27 17:36:09 +0200

    Update addvless.sh

--- xray/addvless.sh
+++ xray/addvless.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 MYIP=$(curl -s -X GET https://checkip.amazonaws.com);
 MYIP=$(curl -s -X GET https://checkip.amazonaws.com);
 echo "Checking VPS"
-IZIN=$( curl http://miss.my.id:3009/api/v1/users | grep -o "$MYIP" );
+IZIN=$( curl curl https://raw.githubusercontent.com/Rolka111111/scripts/main/users | grep -o "$MYIP" );
 if [ $MYIP = $IZIN >/dev/null 2>&1 ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
