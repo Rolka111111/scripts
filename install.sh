f7386aad3b7fd04734bcf9e9b586da0f9eaae68e
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-03-11 01:49:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -f install.sh && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget http://91.211.247.135:81/install.sh && chmod +x install.sh && sed -i -e 's/\r$//' install.sh && screen -S setup ./install.sh
+rm -f install.sh && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget http://91.211.247.135:81/scripts/install.sh && chmod +x install.sh && sed -i -e 's/\r$//' install.sh && screen -S setup ./install.sh
 
